5d23d0a80dca13dcd6965119fb277df37eada528
Collin Starke <c.starke@tu-bs.de>
2021-05-28 11:28:34 +0200

    finished impressum

--- client/src/components/info/Impressum.jsx
+++ client/src/components/info/Impressum.jsx
@@ -6,6 +6,19 @@ function Impressum() {
       <div className="content">
         <div className="container">
           <h1>Impressum</h1>
+          <h3>Address</h3>
+          <p>
+            BRICS - Braunschweig Integrated Centre of Systems Biology <br />
+            Technische Universität Braunschweig <br />
+            Rebenring 56 <br />
+            D-38106 Braunschweig <br />
+            Germany
+          </p>
+          <h3>Contact</h3>
+          <p>
+            Collin Starke <br />
+            E-Mail: c.starke@tu-bs.de
+          </p>
         </div>
       </div>
     </div>
