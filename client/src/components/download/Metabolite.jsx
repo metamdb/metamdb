cbacdd69fcf129ba6b2fd37bcb056f1450e17406
Collin Starke <c.starke@tu-bs.de>
2021-06-18 16:03:21 +0200

    remove unused elements

--- client/src/components/download/Metabolite.jsx
+++ client/src/components/download/Metabolite.jsx
@@ -6,7 +6,6 @@ import { MetaboliteContext } from "../../contexts/MetaboliteContext";
 
 import MetaboliteInfo from "./MetaboliteInfo";
 import MetaboliteIdentifiers from "./MetaboliteIdentifiers";
-import Elements from "./Elements";
 import Reactions from "./Reactions";
 
 const MetaboliteContainer = (props) => {
