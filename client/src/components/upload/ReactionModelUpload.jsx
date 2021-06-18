17158b81c2683fe089f7fbdc304545a4af3d707e
Collin Starke <c.starke@tu-bs.de>
2021-06-18 15:16:37 +0200

    update reaction model text

--- client/src/components/upload/ReactionModelUpload.jsx
+++ client/src/components/upload/ReactionModelUpload.jsx
@@ -13,35 +13,27 @@ const ReactionModelUpload = (props) => {
     <div className="reaction-file-upload">
       <h1>Upload - Reaction Model</h1>
       <p className="lead text-muted">
-        Reaction model upload for atom mapping network generation or labeling
-        simulation. Model must consist of identifiers from{" "}
+        Reaction Models are user-uploaded models of multiple reactions.
+        Reactions and metabolites can be identified by specific database
+        identifiers to get accurate atom mapping data, while manual atom
+        mappings can be used for your custom or simplified reactions.{" "}
         <a
-          href="https://www.brenda-enzymes.org/"
+          href="https://collinstark.github.io/metamdb-docs/reaction-model"
           target="_blank"
           rel="noopener noreferrer"
         >
-          BRENDA
+          You can read about the specifications and more here!
         </a>
-        ,{" "}
-        <a
-          href="https://www.genome.jp/kegg/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          KEGG
-        </a>
-        , or{" "}
-        <a
-          href="https://metacyc.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MetaCyc
-        </a>{" "}
-        in square brackets. Supported file formats include: csv.
       </p>
+      <p>
+        <a href="https://collinstark.github.io/metamdb-docs/assets/files/example_model-89c060de55622eeeb376a7111a2802ef.csv">
+          Download the example model here!
+        </a>
+      </p>
+
       <small className="text-muted">
-        e.g. v1 [10021], Glucose [1], -->, Glucose 6-phosphate [2]
+        e.g. v1 [10021], Glucose [1] (abcdef), -->, Glucose 6-phosphate [2]
+        (abcdef)
       </small>
       <FileUploadContainer {...initialState} />
     </div>
