00f6726e3d55db078c28e3194c64312af1bf8f4c
Collin Starke <c.starke@tu-bs.de>
2021-10-20 13:12:58 +0200

    add webserver config

--- server/config.py
+++ server/config.py
@@ -34,7 +34,7 @@ class Config:
 
     WEBSERVER_PORT = environ.get('WEBSERVER_PORT')
     WEBSERVER_HOST = environ.get('WEBSERVER_HOST')
-    WEBSERVER_URI = '{0}:{1}'.format(WEBSERVER_HOST, WEBSERVER_PORT)
+    WEBSERVER_URI = environ.get('WEBSERVER_URI')
 
     ORCID_CLIENT_ID = environ.get('ORCID_CLIENT_ID')
     ORCID_CLIENT_SECRET = environ.get('ORCID_CLIENT_SECRET')
