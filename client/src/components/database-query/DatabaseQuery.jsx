8e2647d385179279b4ae3745190b8c96ff75c06a
Collin Starke <c.starke@tu-bs.de>
2021-10-27 16:13:51 +0200

    remove unnecessary import

--- client/src/components/database-query/DatabaseQuery.jsx
+++ client/src/components/database-query/DatabaseQuery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import classnames from "classnames";
 import axios from "axios";
 import qs from "qs";
